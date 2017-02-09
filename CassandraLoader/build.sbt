d2ff25599cfdcf85be289b064b6f5225ab6d3e27
Patryk Sulewski <sulewskip@gmail.com>
2017-02-09 10:00:23 +0100

    Initial commit to repo

---
+++ CassandraLoader/build.sbt
@@ -0,0 +1,6 @@
+name := "CassandraLoader"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+
---
+++ CassandraLoader/src/main/scala/testObj.scala
@@ -0,0 +1,6 @@
+/**
+  * Created by pps on 2017-02-09.
+  */
+object testObj extends App{
+  println("Hello world")
+}
