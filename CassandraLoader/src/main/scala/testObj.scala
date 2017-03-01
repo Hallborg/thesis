ef1a1add3a41f34b1b02df2b16a5d73937933f31
Patryk Sulewski <sulewskip@gmail.com>
2017-03-01 13:37:39 +0100

    Reading of jason data to cassandra doneish

--- CassandraLoader/src/main/scala/testObj.scala
+++ CassandraLoader/src/main/scala/testObj.scala
@@ -1,12 +1,5 @@
 
 import scala.io.Source
-import java.io.InputStream
-
-import com.fasterxml.jackson.annotation.JsonValue
-
-//import play.api.libs.iteratee.Execution.Implicits.defaultExecutionContext
-///import play.api.libs.json.jackson.JacksonJson
-//import org.apache.flink.api.scala._
 import play.api.libs.json.Json
 import play.api.libs.json._
 
@@ -19,59 +12,44 @@ object testObj {
     val con1 = new CassandraClientClass(53003)
 
     val source: String = Source.fromFile("../dataModel/call_event.json").getLines.mkString
-    val json: JsValue = Json.parse(source)
+    val json: List[JsValue] = Json.parse(source).as[List[JsValue]]
     val json_str: String = json.toString()
-    //println(json_str.charAt(50)
-    //convert(json\("edr")\("service"))
-    check_service(json\("edr")\("service"), json)
-    //check_service(convert(json\("edr")\("service")))
-    //println(json_str.contains("\"service\":\"1\""))
-    //con1.execSession(
-    //  "INSERT INTO cdr.edr JSON '%s!' ".format(json)
-    //)
+    /*for (i <- 0 to json.length-1) {
+      val value = json.apply(i)
+      check_service(value \ ("edr") \ ("service"), json.apply(i))
+    }*/
+    json.foreach(executeQuery(_, con1))
+    // "INSERT INTO cdr.edr JSON '%s!'" ;
     con1.closeCon()
-    //con1.execSession(
-    //  "TRUNCATE myk.users ;"
-    //)
-    //con1.insertValueFromCassandraTable()
-    //println(con1.getValueFromCassandraTable())
 
-   /* val con2 = new CassandraClientClass(53004)
-    con2.insertValueFromCassandraTable2()
-    println(con2.getValueFromCassandraTable())
-
-    val con3 = new CassandraClientClass(53005)
-    con3.insertValueFromCassandraTable3()
-    println(con3.getValueFromCassandraTable())
-
-
-    println(con1.execSession(
-      "SELECT * FROM myk.users WHERE id='2' ; "
-    ))
-    con1.closeCon()
-    println(con2.execSession(
-      "SELECT * FROM myk.users WHERE id='3' ; "
-    ))
-    con2.closeCon()
-    println(con3.execSession(
-      "SELECT * FROM myk.users WHERE id='1' ;"
-    ))
-    con3.closeCon()
-    //CassandraClient.insertValueFromCassandraTable()
-    //println(CassandraClient.getValueFromCassandraTable())
-    //CassandraClient.closeCon()*/
   }
 
-  def check_service(service: JsValue, json: JsValue): Unit = {
+  def executeQuery(json: JsValue, con: CassandraClientClass): Unit = {
+    val json_part = check_service(json \ ("edr") \ ("service"), json)
+    println(json_part)
+
+    con.execSession(
+      "INSERT INTO cdr.edr JSON '%s!'".format(json_part)
+    )
+  }
+
+  def check_service(service: JsValue, json: JsValue): String = {
     convert(service) match { //convert(service)
       case 1 => call_event(json)
-      case 0 => println("service 0")
+      case 2 => data_event(json)
     }
   }
   def convert(value: JsValue): Int = {
     value.as[String].toInt
   }
-  def call_event(json: JsValue): Unit = {
-    println("service 1")
+  def call_event(json: JsValue): String = {
+    val json_string = (json\("edr")).toString()
+    //val temp = json_string.replaceAll("-", "_")
+    json_string.replace("\"event_details\"", "\"call_event\"")
+  }
+  def data_event(json: JsValue): String = {
+    val json_string = (json\("edr")).toString()
+    //val temp = json_string.replaceAll("-", "_")
+    json_string.replace("\"event_details\"", "\"data_event\"")
   }
 }
