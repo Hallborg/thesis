795a56b79976338b9a9d32e18e5ee490d88eb4d5
Jesper <Jesper.Hallis@gmail.com>
2017-03-09 12:45:21 +0100

    Load/deload json data from queue

--- queue-scripts/recieve.py
+++ queue-scripts/recieve.py
@@ -2,18 +2,21 @@
 
 import pika
 import sys
+import json
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='localhost', port=32772))
+        host='localhost', port=32769))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='edr')
 
 def callback(ch, method, properties, body):
     print(" [x] Received %r" % body)
+    j_bod = json.loads(body)
+    print(j_bod["edr"]["id"])
 
 channel.basic_consume(callback,
-                      queue='hello',
+                      queue='edr',
                       no_ack=True)
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
--- queue-scripts/send.py
+++ queue-scripts/send.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python
 import pika
 import sys
+import json
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='localhost', port=32772))
+        host='localhost', port=32769))
 channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+channel.queue_declare(queue='edr')
+with open("../dataModel/call_event.json") as source_block:
+    call_events = json.load(source_block)
 
-channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body='Hello Patryk!')
-print(" [x] Sent 'Hello World!'")
+for item in call_events:
+    print item["edr"]["id"]
+    channel.basic_publish(exchange='',
+                         routing_key='edr',
+                         body=json.dumps(item))
+    print(" [x] Sent one EDR")
 
 connection.close()
