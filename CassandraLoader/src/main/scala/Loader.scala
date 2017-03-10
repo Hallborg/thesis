630f713540485b1798c7f1d63b124ebabdbeae1e
Jesper <Jesper.Hallis@gmail.com>
2017-03-10 13:08:10 +0100

    Max-load and warm up for sending data.

--- CassandraLoader/src/main/scala/Loader.scala
+++ CassandraLoader/src/main/scala/Loader.scala
@@ -1,17 +1,48 @@
 import play.api.libs.json.{JsValue, Json}
-
+import sys.process._
 import scala.io.Source
+import java.io._
 
 /**
   * Created by Hallborg on 2017-03-09.
   */
-class Loader(filePath: String, port: Int) extends Runnable {
+class Loader(setting: Int, filePath: String, port: Int) extends Runnable {
   def run() = {
     val con = new CassandraClientClass(port)
     val source: String = Source.fromFile(filePath).getLines.mkString
     val json_data: List[JsValue] = Json.parse(source).as[List[JsValue]]
 
-    json_data.foreach(Importer.executeQuery(_, con))
+    var text_s = "Strsss test started"
+    var text_f = "Stress test stopped"
+
+
+    val start_date = "date +%s000000000" !!;
+    if (setting == 0) {
+      json_data.foreach(Importer.executeQuery(_, con))
+    }
+    else if (setting == 1) {
+      text_s = "Step-wise test started"
+      text_f = "Step-wise test stopped"
+      var start = 0
+      var end = 2
+      while (end < json_data.size) {
+        json_data.slice(start, end) foreach (Importer.executeQuery(_, con))
+        start = end
+        end = end * 2
+        Thread.sleep(500)
+      }
+      json_data.slice(start, json_data.size) foreach (Importer.executeQuery(_, con))
+    }
+
+    val end_date = "date +%s000000"!!;
+    val pw = new PrintWriter(new File("../thesis-scripts/" + Thread.currentThread().getName))
+
+    pw.write("alert text='%s' %s".format(text_s, start_date))
+    pw.write("alert text='%s' %s".format(text_f, end_date))
+
     con.closeCon()
+    pw.close()
+
+
   }
 }
--- CassandraLoader/src/main/scala/testObj.scala
+++ CassandraLoader/src/main/scala/testObj.scala
@@ -2,12 +2,16 @@
 /**
   * Created by pps on 2017-02-09.
   */
+
 object testObj {
   def main(args: Array[String]): Unit = {
 
     // Running one thread at the moment. Waiting for three seperate files to load.
-    new Thread(new Loader("../dataModel/call_event.json", 53003)).start
+
+
+    new Thread(new Loader(1, "../dataModel/mockdata.json", 53003)).start
     // new Thread(new Loader("../dataModel/call_event.json", 53004)).start
     // new Thread(new Loader("../dataModel/call_event.json", 53005)).start
+
   }
 }
