0dde4271c892d5a67837ec3e60d8498ee0fe9fda
Jesper Hallborg <jesper.hallis@gmail.com>
2017-03-20 15:36:31 +0000

    Update apt-install.sh

--- thesis-scripts/apt-install.sh
+++ thesis-scripts/apt-install.sh
@@ -12,6 +12,7 @@ apt-get install -y \
 echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee /etc/apt/sources.list.d/webupd8team-java.list;
 echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list;
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886;
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections;
 
 # Docker
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -;
