8e0f9d7d5ab2eb51f5c4f7835b5ef19cf420aa08
Patryk Sulewski <sulewskip@gmail.com>
2017-03-27 14:59:06 +0200

    Fixed docker scripts, works like a charm.

--- docker-scripts/startNode1.sh
+++ docker-scripts/startNode1.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DIR2="$DIR/../cassandra-models"
+my_ip=$(ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
 
-docker run --name $(hostname)-docker -d --net=host -p 7000:7000 -p 9042:9042 -e CASSANDRA_BROADCAST_ADDRESS=192.168.46.11 -v $DIR2:/cassandra-models cassandra:3.9 #~/thesis/cassandra-models:/cassandra-models
+docker run --name $(hostname)-docker -d --net=host -p 7000:7000 -p 9042:9042 \
+-e CASSANDRA_BROADCAST_ADDRESS=$my_ip -e CASSANDRA_LISTEN_ADDRESS=$my_ip -e \
+CASSANDRA_RPC_ADDRESS=$my_ip -e CASSANDRA_ENDPOINT_SNITCH=GossipingPropertyFileSnitch \
+-e LOCAL_JMX=no -e JVM_OPTS=-Djava.rmi.server.hostname=$my_ip -v ~/doc-cassandra:/var/lib/cassandra \
+jmx-cassandra:1
 sleep 60
 docker exec cont1 cqlsh -e "SOURCE '../cassandra-models/edr.cql'"
+
+#docker run --name $(hostname)-docker -d --net=host -p 7000:7000 -p 9042:9042 -e CASSANDRA_BROADCAST_ADDRESS=192.168.46.11 -v $DIR2:/cassandra-models cassandra:3.9 #~/thesis/cassandra-models:/cassandra-models
 #docker run --name cont2 -d -p 53004:9042 -e CASSANDRA_SEEDS="$(docker inspect --format='{{ .NetworkSettings.IPAddress }}' cont1)" cassandra:3
 #sleep 60
 #docker run --name cont3 -d -p 53005:9042 -e CASSANDRA_SEEDS="$(docker inspect --format='{{ .NetworkSettings.IPAddress }}' cont2)" cassandra:3
