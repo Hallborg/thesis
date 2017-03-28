b1b25ef86fb78b7fde5a84a68fbde8ee5da2d9a0
Jesper <Jesper.Hallis@gmail.com>
2017-03-28 11:20:35 +0200

    Another fix

--- CassandraLoader/src/main/scala/testObj.scala
+++ CassandraLoader/src/main/scala/testObj.scala
@@ -13,33 +13,23 @@ object testObj {
 
     // Running one thread at the moment. Waiting for three seperate files to load.
     // args(0), 0 is for full load, 1 is for step-wise load
-    
-    if ( args.size == 3 ) {
-      println("python ~/thesis/dataModel/data-generator.py %s".format(args(2)) !!)
-    }
-    //val l1 = new Loader(args(0).toInt,"Thread-1", "../dataModel/mockdata-0.json", "53003")
-    //val l2 = new Loader(args(0).toInt,"Thread-2", "../dataModel/mockdata-1.json", "53004")
-    //val l3 = new Loader(args(0).toInt,"Thread-3", "../dataModel/mockdata-2.json", "53005")
-    //val l4 = new Loader(args(0).toInt,"Thread-4", "../dataModel/mockdata-3.json", "53005")
 
-    val l1 = new Loader(args(0).toInt,"Thread-1", "~/thesis/dataModel/mockdata-0.json", args(1))
-    val l2 = new Loader(args(0).toInt,"Thread-2", "~/thesis/dataModel/mockdata-1.json", args(1))
-    val l3 = new Loader(args(0).toInt,"Thread-3", "~/thesis/dataModel/mockdata-2.json", args(1))
-    val l4 = new Loader(args(0).toInt,"Thread-4", "~/thesis/dataModel/mockdata-3.json", args(1))
+    val loaders = create_loaders(args)
+
     
     val f1 = Future {
-      l1.run_separate()
+      loaders(0).run_separate()
     }
     val f2 = Future {
-      l2.run_separate()
+      loaders(1).run_separate()
     }
 
     val f3 = Future {
-      l3.run_separate()
+      loaders(2).run_separate()
     }
 
     val f4 = Future {
-      l4.run_separate()
+      loaders(3).run_separate()
     }
 
     println("main thread blocked")
@@ -49,10 +39,10 @@ object testObj {
 
     clear_keeper(Await.result(f1, 60 minute), Await.result(f2, 60 minute), Await.result(f3, 60 minute), Await.result(f4, 60 minute))
     println("main thread unblocked")
-    val f11 = Future { l1.run_mix() }
-    val f12 = Future { l2.run_mix() }
-    val f13 = Future { l3.run_mix() }
-    val f14 = Future { l4.run_mix() }
+    val f11 = Future { loaders(0).run_mix() }
+    val f12 = Future { loaders(1).run_mix() }
+    val f13 = Future { loaders(2).run_mix() }
+    val f14 = Future { loaders(3).run_mix() }
    
     val r1 = Await.result(f11, 60 minute)
     Await.result(f12, 60 minute).closeCon()
@@ -73,5 +63,21 @@ object testObj {
     println("Truncating database")
     r1.truncate()
     Thread.sleep(5000)
+  }
+
+  def create_loaders(args: Array[String]): List[Loader] = {
+    if ( args.size == 3 ) {
+      println("python ~/thesis/dataModel/data-generator.py %s".format(args(2)) !!)
+      List(new Loader(args(0).toInt,"Thread-1", "~/thesis/dataModel/mockdata-0.json", args(1)),
+      new Loader(args(0).toInt,"Thread-2", "~/thesis/dataModel/mockdata-1.json", args(1)),
+      new Loader(args(0).toInt,"Thread-3", "~/thesis/dataModel/mockdata-2.json", args(1)),
+      new Loader(args(0).toInt,"Thread-4", "~/thesis/dataModel/mockdata-3.json", args(1)))
+    }
+    else {
+      List(new Loader(args(0).toInt,"Thread-1", "../dataModel/mockdata-0.json", args(1)),
+      new Loader(args(0).toInt,"Thread-2", "../dataModel/mockdata-1.json", args(1)),
+      new Loader(args(0).toInt,"Thread-3", "../dataModel/mockdata-2.json", args(1)),
+      new Loader(args(0).toInt,"Thread-4", "../dataModel/mockdata-3.json", args(1)))
+    }
   }
 }
