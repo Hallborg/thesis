b63c2f985183206e95507e8ce3450b3a53d8e068
Jesper Hallborg <jesper.hallis@gmail.com>
2017-03-30 12:20:05 +0000

    Update startNodes.sh

--- docker-scripts/startNodes.sh
+++ docker-scripts/startNodes.sh
@@ -2,9 +2,14 @@
 my_ip=$(ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
 first_ip=$(arp -a | head -n 1 | awk '{print $2}' | cut -c 2- | rev | cut -c 2- | rev)
 
+volume="-v /root/thesis/doc-cassandra:/var/lib/cassandra"
+if [ "$1" = "i" ]; then
+    volume=""
+fi
+
 docker run --name $(hostname)-docker -d --net=host -p 7000:7000 -p 9042:9042 \
 -e CASSANDRA_BROADCAST_ADDRESS=$my_ip -e CASSANDRA_SEEDS=$first_ip \
 -e CASSANDRA_LISTEN_ADDRESS=$my_ip -e CASSANDRA_RPC_ADDRESS=$my_ip \
 -e CASSANDRA_ENDPOINT_SNITCH=GossipingPropertyFileSnitch -e LOCAL_JMX=no \
--e JVM_OPTS=-Djava.rmi.server.hostname=$my_ip -v ~/doc-cassandra:/var/lib/cassandra \
-jmx-cassandra:1
+-e JVM_OPTS=-Djava.rmi.server.hostname=$my_ip $volume \
+laban/jxm-cassandra:1
